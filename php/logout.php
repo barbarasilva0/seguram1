ff3d32864492dd62b6fffb0cf8b08263790f1400
barbarasilva0 <barbarasilva02003@gmail.com>
2025-04-19 23:49:53 +0100

    Delete php directory

--- php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove todas as variáveis de sessão
-session_destroy(); // Destroi a sessão
-
-// Redireciona para a página de login
-header("Location: ../index.html");
-exit();
-?>
